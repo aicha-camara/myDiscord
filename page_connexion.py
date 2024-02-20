467bfab49bfab2b0e9b0bf74e510709f1db691cc
camara_aicha <aicha.ca@outlook.fr>
2024-02-20 21:35:12 +0100

    J'ai rajouté un systeme de verification d'utilisateur : le fonction permet que lorsque un user veut se connecter on va verifier si il est dans la base de donnée sinon il rentre pas dans le programme

--- page_connexion.py
+++ page_connexion.py
@@ -1,6 +1,7 @@
 import tkinter
 import customtkinter
 import subprocess
+from database import Identification
 from PIL import ImageTk,Image
 
 
@@ -8,22 +9,46 @@ customtkinter.set_appearance_mode("System")  # Modes: system (default), light, d
 customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
 
 
-app = customtkinter.CTk()  #creating cutstom tkinter window
+app = customtkinter.CTk()
 app.geometry("1200x640")
 app.title('Login')
 
 
-
 def button_function():
-    app.destroy()            # destroy current window and creating new one 
-    w = customtkinter.CTk()  
-    w.geometry("1280x720")
-    w.title('Welcome')
-    l1=customtkinter.CTkLabel(master=w, text="Home Page",font=('Century Gothic',60))
-    l1.place(relx=0.5, rely=0.5,  anchor=tkinter.CENTER)
-    subprocess.Popen(["python", "main.py"])
+    # Récupérer le pseudo et le mot de passe saisis par l'utilisateur
+    pseudo_entre = entry1.get()
+    mot_de_passe_entre = entry2.get()
 
-    
+    # Rechercher dans la base de données si les informations d'identification sont valides
+    identification = Identification(host="localhost", user="root", password="za9?-U5zwD4-6#L", database="user")
+    liste_utilisateurs = identification.liste_user()
+    identification.fermer_connexion()
+
+    pseudo_mot_de_passe_valides = False
+    for utilisateur in liste_utilisateurs:
+        if utilisateur[1] == pseudo_entre and utilisateur[2] == mot_de_passe_entre:
+            pseudo_mot_de_passe_valides = True
+            break
+
+    if pseudo_mot_de_passe_valides:
+        # Détruire la fenêtre de connexion
+        app.destroy()
+
+        # Créer une nouvelle fenêtre de bienvenue
+        w = customtkinter.CTk()
+        w.geometry("1280x720")
+        w.title('Welcome')
+
+        # Ajouter un label à la fenêtre de bienvenue
+        l1 = customtkinter.CTkLabel(master=w, text="Home Page", font=('Century Gothic', 60))
+        l1.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+        # Lancer le fichier principal (main.py) dans un nouveau processus
+        subprocess.Popen(["python", "main.py"])
+    else:
+        # Informer l'utilisateur que les informations d'identification sont incorrectes
+        print("Pseudo ou mot de passe incorrect.")
+        # Vous pouvez également afficher un message d'erreur à l'utilisateur ou réinitialiser les champs de saisie.
 
 
 img1=ImageTk.PhotoImage(Image.open("assets/pattern.png"))
