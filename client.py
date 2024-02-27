ce445d080d492dd0f7a614e5b33b7e983fccd123
camara_aicha <aicha.ca@outlook.fr>
2024-02-27 18:34:57 +0100

    l'ajout de la methode recevoir il est pas fonctionnel,c'est un prototype car le fichier n'est pas au complet

--- client.py
+++ client.py
@@ -2,6 +2,19 @@ import socket
 import threading
 
 pseudo = input('rentrer un pseudo')
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('10.10.95.178', 12345))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('10.10.95.178', 12345))
+
+def recevoir():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'pseudo':
+                client.send(pseudo.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("il ya une erreur")
+            client.close()
+            break
 
