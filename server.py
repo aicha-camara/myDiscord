e6ebb8c0cb64a8d7037b43ed35b061c45b7a6a9a
camara_aicha <aicha.ca@outlook.fr>
2024-02-27 17:35:22 +0100

    L'ajout de la methode recevoir qui permet de recevoir les notification de reception des clients

--- server.py
+++ server.py
@@ -1,29 +1,50 @@
 import socket
+import threading
 
-host = '192.168.1.166'
+hote = '10.10.95.178'
 port = 12345
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server_socket.bind((hote, port))
 server_socket.listen(5)
 
 clients = []
-pseudo = []
+pseudos = []
 
-def broadcast(message):
+
+def diffusion(message):
     for client in clients:
         client.send(message)
 
-def handle(client):
+
+def gerer(client):
     while True:
         try:
             message = client.recv(1024)
-            broadcast(message)
+            diffusion(message)
         except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
-            pseudo = pseudo[index]
-            broadcast(f'{pseudo} a quitter la discussion'.encode('ascii'))
-            pseudo.remove(pseudo)
+            pseudo = pseudos[index]
+            diffusion(f'{pseudo} a quitté la discussion'.encode('utf-8'))
+            pseudos.remove(pseudo)
             break
+
+
+def recevoir():
+    while True:
+        client, adresse = server_socket.accept()
+        print(f"connecté avec {str(adresse)}")
+        client.send('pseudo?'.encode('utf-8'))
+        pseudo = client.recv(1024).decode('utf-8')
+        pseudos.append(pseudo)
+        clients.append(client)
+        print(f"Le pseudo du client est {pseudo}")
+        diffusion(f'{pseudo} a rejoint le chat'.encode('utf-8'))
+        thread = threading.Thread(target=gerer, args=(client,))
+        thread.start()
+
+
+print("serveur en écoute")
+recevoir()
