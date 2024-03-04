518115f22acbd6789f3c6be575502b1210ff94c4
fatou-diaby <146712224+fatou-diaby@users.noreply.github.com>
2024-03-04 21:27:16 +0100

    Creation de la methode login qui permet de se connecter et rentrer dans l'application

--- page_connexion.py
+++ page_connexion.py
@@ -13,5 +13,17 @@ app = customtkinter.CTk()
 app.geometry("1200x640")
 app.title('Login')
 
+def login():
+    pseudo_entre = entry1.get()
+    mot_de_passe_entre = entry2.get()
 
+    identification = Utilisateur(host="localhost", user="root", password="e~KPh75=6p[G", database="myDiscord")
+    liste_utilisateurs = identification.liste_user()
+    identification.fermer_connexion()
+
+    pseudo_mot_de_passe_valides = False
+    for utilisateur in liste_utilisateurs:
+        if utilisateur[5] == pseudo_entre and utilisateur[4] == mot_de_passe_entre:
+            pseudo_mot_de_passe_valides = True
+            break
 
