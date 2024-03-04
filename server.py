389bc27a069472c4b6e11eea5459e3c0b365eafa
camara_aicha <aicha.ca@outlook.fr>
2024-03-04 15:26:35 +0100

    jai changé la valeur de l'ip

--- server.py
+++ server.py
@@ -1,54 +1,35 @@
 import socket
 import threading
 
-
-class Serveur:
-    def __init__(self, hote="10.10.98.248", port=55555):
+class ClientSocket:
+    def __init__(self, hote="10.10.103.118", port=55556):
         self.hote = hote
         self.port = port
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.hote, self.port))
-        self.clients = []
-        self.pseudos = []
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.hote, self.port))
 
-    def diffusion(self, message):
-        for client in self.clients:
-            client.send(message)
-
-    def gerer_client(self, client):
+    def recevoir(self, pseudo):
         while True:
             try:
-                message = client.recv(1024)
-                self.diffusion(message)
+                message = self.client.recv(1024).decode('utf-8')
+                if message == 'pseudo':
+                    self.client.send(pseudo.encode('utf-8'))
+                else:
+                    print(message)
             except Exception as e:
-                print(f"Erreur lors du traitement du client : {e}")
-                index = self.clients.index(client)
-                self.clients.remove(client)
-                client.close()
-                pseudo = self.pseudos[index]
-                self.diffusion(f'{pseudo} a quitté la discussion'.encode('utf-8'))
-                self.pseudos.remove(pseudo)
+                print(f"Il y a une erreur : {e}")
+                self.client.close()
                 break
 
-    def recevoir_clients(self):
+    def write(self, pseudo):
         while True:
-            client, adresse = self.server_socket.accept()
-            print(f"Connecté avec {str(adresse)}")
-            client.send('pseudo'.encode('utf-8'))
-            pseudo = client.recv(1024).decode('utf-8')
-            self.pseudos.append(pseudo)
-            self.clients.append(client)
-            print(f"Le pseudo du client est {pseudo}")
-            self.diffusion(f'{pseudo} a rejoint le chat'.encode('utf-8'))
-            thread = threading.Thread(target=self.gerer_client, args=(client,))
-            thread.start()
-
-    def demarrer(self):
-        print("Serveur en écoute")
-        self.server_socket.bind((self.hote, self.port))
-        self.server_socket.listen(5)
-        self.recevoir_clients()
+            message = f'{pseudo}: {input("")}'
+            self.client.send(message.encode('utf-8'))
 
 if __name__ == "__main__":
-    serveur = Serveur()  # Créer une instance de la classe Serveur
-    serveur.demarrer()
+    pseudo = input("Rentrer un pseudo : ")
+    client_socket = ClientSocket()
+    receive_thread = threading.Thread(target=client_socket.recevoir, args=(pseudo,))
+    receive_thread.start()
+    write_thread = threading.Thread(target=client_socket.write, args=(pseudo,))
+    write_thread.start()
