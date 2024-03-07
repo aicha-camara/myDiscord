f609b7488263982e34ea36cea3af7df87fac2c4e
fatou-diaby <146712224+fatou-diaby@users.noreply.github.com>
2024-03-07 19:23:34 +0100

    ajout de la methode afficher_channel qui permet d'afficher les channel de la base de donnée

--- test2.py
+++ test2.py
@@ -1,1 +1,13 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import os
+from database import Utilisateur
+
+def afficher_channels():
+    identification = Utilisateur(host="localhost", user="root", password="e~KPh75=6p[G", database="myDiscord")
+    liste_channels = identification.liste_channel()
+    identification.fermer_connexion()
+
+    for channel in liste_channels:    
+        print(f"Vous avez accès au channel : {channel[0]}")
 
